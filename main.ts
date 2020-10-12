498c14cdae7e14c49c2a820f2cb90afd812552c6
MarcoOjedaA01285307 <70342450+MarcoOjedaA01285307@users.noreply.github.com>
2020-10-11 22:06:41 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,75 @@
+input.onPinPressed(TouchPin.P0, function () {
+    for (let index = 0; index < 15; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            . . # . #
+            # # . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . # # .
+            # # . . #
+            `)
+        basic.clearScreen()
+    }
+    for (let index = 0; index < 15; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . # . # .
+            . # . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    X = 0
+    for (let X = 0; X <= 4; X++) {
+        led.plot(X, 0)
+        basic.pause(100)
+        led.unplot(X, 0)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    counter = randint(0, 15)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+})
+let counter = 0
+let X = 0
+for (let index = 0; index < 4; index++) {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.West)
+}
+basic.clearScreen()
